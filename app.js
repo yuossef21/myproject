96927cc4338ee2a2b8c0e75d332023014078fda3
yuossef21 <155336029+yuossef21@users.noreply.github.com>
2024-02-28 19:23:45 +0300

    Update app.js

---
+++ app.js
@@ -0,0 +1,11 @@
+let img2 = document.querySelector(".img2");
+let containr = document.querySelector(".containr");
+
+function phones(phone) {
+  img2.src = phone;
+}
+function colers(coler) {
+    containr.style.background = coler;
+
+    
+}
